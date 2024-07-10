15a54d5f796c462c53c60ed07cdd562439292d84
Manoj Singh Adhikari <75062320+Adhikari1606@users.noreply.github.com>
2024-07-10 13:57:02 +0530

    Time: 1 ms (99.42%) | Memory: 41.9 MB (51.62%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        int ctr=0;
+        Map<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<logs.length;i++)
+        {
+            if(logs[i].equals("../"))
+            {
+                if(ctr>0)
+                ctr--;
+            }
+            else if(logs[i].equals("./"))
+            {
+                continue;
+            }
+            else
+            {
+                ctr++;
+            }
+        }
+        return ctr;
+    }
+}
