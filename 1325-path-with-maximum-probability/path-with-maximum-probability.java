81f1c7ed8f1ef084da7d0e3720dc51ffdb66e819
Manoj Singh Adhikari <75062320+Adhikari1606@users.noreply.github.com>
2024-08-27 17:35:58 +0530

    Time: 45 ms (41.00%) | Memory: 55.3 MB (100.00%) - LeetSync

---
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+class Pair {
+    int node;
+    float weight;
+
+    Pair(int node, float weight) {
+        this.node = node;
+        this.weight = weight;
+    }
+}
+
+class Solution {
+    public void Bfs(int s, List<List<Pair>> adjList, float[] dist) {
+        PriorityQueue<Pair> pq = new PriorityQueue<>(Comparator.comparingDouble(p -> -p.weight));
+        pq.add(new Pair(s, 1.0f));
+
+        while (!pq.isEmpty()) {
+            Pair current = pq.poll();
+            int u = current.node;
+            float currentDist = current.weight;
+
+            for (Pair neighbor : adjList.get(u)) {
+                int v = neighbor.node;
+                float weight = neighbor.weight;
+
+                if (currentDist * weight > dist[v]) {
+                    dist[v] = currentDist * weight;
+                    pq.add(new Pair(v, dist[v]));
+                }
+            }
+        }
+    }
+
+    public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
+        float[] arr = new float[n];
+        Arrays.fill(arr, 0.0f);
+        arr[start_node] = 1.0f;
+
+        List<List<Pair>> adjList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < edges.length; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            float prob = (float) succProb[i];
+            adjList.get(u).add(new Pair(v, prob));
+            adjList.get(v).add(new Pair(u, prob));
+        }
+
+        Bfs(start_node, adjList, arr);
+
+        return arr[end_node];
+    }
+}
