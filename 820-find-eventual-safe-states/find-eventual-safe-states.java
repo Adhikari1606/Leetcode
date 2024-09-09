9dfdc7e163639e6ad0fe11f1a4cdf5602f917fa2
Manoj Singh Adhikari <75062320+Adhikari1606@users.noreply.github.com>
2024-09-09 22:09:07 +0530

    Time: 4 ms (98.93%) | Memory: 55 MB (94.45%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,8 +1,37 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
+   public boolean dfs(int a, int[][] graph, int[] visited, int[] pathvisited) {
+        visited[a] = 1;
+        pathvisited[a] = 1;
+        
+        for (int it : graph[a]) {
+            if (visited[it] == 0) {
+                if (!dfs(it, graph, visited, pathvisited)) {
+                    return false;
+                }
+            } else if (pathvisited[it] == 1) {
+                return false;
+            }
+        }
+
+        pathvisited[a] = 0;
+        return true;
+    }
+
     public List<Integer> eventualSafeNodes(int[][] graph) {
+        List<Integer> safeNodes = new ArrayList<>();
+        int[] visited = new int[graph.length];
+        int[] pathvisited = new int[graph.length];
+        
+        for (int i = 0; i < graph.length; i++) {
+            if ( dfs(i, graph, visited, pathvisited)) {
+                safeNodes.add(i);
+            }
+        }
+        
+        return safeNodes;
+    }
+
+    /*public List<Integer> eventualSafeNodes(int[][] graph) {
         int len = graph.length;
         List<Integer> result = new ArrayList<>();
         int[] state = new int[len];
@@ -23,5 +52,5 @@ class Solution {
         }
         state[node] = 2;
         return true;
-    }
+    }*/
 }
